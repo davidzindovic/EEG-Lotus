124083eb37ac9fbee0344d477c559e3047198513
David Zindović <96695748+davidzindovic@users.noreply.github.com>
2024-06-07 12:10:26 +0200

    Update EEG_Roza.ino

--- EEG_Roza.ino
+++ EEG_Roza.ino
@@ -18,17 +18,21 @@
 #define DEMO_LED 12
 #define END_SWITCH 13
 #define HALL_SENSOR A0
-
+/*
 #define HITROST_MOTORJA 2000
 #define KORAKI_MOTORJA 100 //?preveri!
-#define MAX_KORAKI_MOTORJA 10000 //popravi!
-
+#define MAX_POZICIJA_MOTORJA 10000 //popravi!
+*/
 #define BAUDRATE 57600
 
 #define SAFETY_CAKANJE 2000 //čas po odložitvi v ms
+#define DEMO_CAKANJE 1000   //čas pavze pri demo načinu
+//uint32_t DEMO_CAKANJE=1000;
 
 //za ne debugiranje moraš zakomentirati spodnjo vrstico
-#define DEBUG 0
+#define DEBUG_GENERIC 0
+#define DEBUG_MOTOR 0
+#define DEBUG_EEG 0
 
 SoftwareSerial bluetooth(A2,8);
 //Mindwave mindwave;
@@ -80,6 +84,7 @@ void setup() {
 
   pinMode(HALL_SENSOR,INPUT);
   pinMode(END_SWITCH,INPUT_PULLUP);
+  
   bluetooth.begin(BAUDRATE);
   //mindwave.setupe();
   Serial.begin(BAUDRATE);
@@ -96,18 +101,31 @@ static bool first_nastavitve=1;
 
 if(first_nastavitve){nastavitve=ui_config();first_nastavitve=!first_nastavitve;}
 
-while(analogRead(HALL_SENSOR)<=(HALL_SENSOR_AVRG-HALL_SENSOR_WIGGLE_ROOM)){Serial.println("CAKAM DA ODLOZIS");} //dokler je UI plošča odmaknjena naprava čaka
+while(analogRead(HALL_SENSOR)<=(HALL_SENSOR_AVRG-HALL_SENSOR_WIGGLE_ROOM)){
+  #if DEBUG_GENERIC
+  Serial.print("CAKAM DA ODLOZIS UI PLATO | HALL_SENOZOR:");
+  Serial.print(analogRead(HALL_SENSOR));
+  Serial.print("Povprečje HALL: ");
+  Serial.print(HALL_SENSOR_AVRG);
+  Serial.print(" +/- ");
+  Serial.println(HALL_SENSOR_WIGGLE_ROOM);
+  #endif
+  } //dokler je UI plošča odmaknjena naprava čaka
 
 delay(SAFETY_CAKANJE); // po potrditvi konfiguracije in odložitvi ploščice ima uporabnik 10 sekund da umakne roko
 
 while((analogRead(HALL_SENSOR)>=(HALL_SENSOR_AVRG-HALL_SENSOR_WIGGLE_ROOM)) ){
-Serial.println("GARBAM  ");//Serial.println(bluetooth.available());
-//Serial.print((nastavitve&mask_demo)==4);
-//Serial.print((nastavitve&mask_log)==2);Serial.println((nastavitve&mask_roza)==1); 
-
+#if DEBUG_GENERIC
+Serial.print("GARBAM  parametri:");
+Serial.print((nastavitve&mask_demo)==4);
+Serial.print((nastavitve&mask_log)==2);
+Serial.println((nastavitve&mask_roza)==1); 
+#endif
 ObdelavaPodatkov((nastavitve&mask_demo)==4,(nastavitve&mask_log)==2,(nastavitve&mask_roza)==1);
 }
+#if DEBUG_GENERIC
 Serial.println("NE GARBAM");
+#endif
 if((nastavitve&mask_log)==2)Serial.println("stop"); // če smo merili pošjemo stop da python neha beležiti podatke
 for(int a=0;a<ST_POVPRECIJ;a++)meritve[a]=0;
 nastavitve=ui_config();
@@ -130,7 +148,7 @@ byte ReadOneByte() {
   while(!bluetooth.available());
   ByteRead = bluetooth.read();
 
-#if DEBUG 
+#if DEBUG_EEG
   Serial.println(ByteRead);   // echo the same byte out the USB serial (for debug purposes)
 #endif
 
@@ -189,7 +207,7 @@ void EEG()
           } // switch
         } // for loop
 
-#if DEBUG
+#if DEBUG_EEG
 
         // *** Add your code here ***
 
@@ -213,15 +231,19 @@ void EEG()
     } // end if read 0xAA byte
   } // end if read 0xAA byte
 }
+//----------------------------------------------------------------------------
 // funkcija za obdelavo podatkov:
 void ObdelavaPodatkov(bool demo,bool ali_merim, bool odpiranje) {
   //demo mode spremenljivki:
   static bool demo_flag=0; //če sem prišel do zgornje ali spodnje meje
-  static bool smer=0;      // da lahko spreminjam smer vrtejna v demo mode
+  static bool smer=1;      // da lahko spreminjam smer vrtejna v demo mode
  
  // static uint8_t meritve[ST_POVPRECIJ];
   static byte stevec_meritev=0;
   static bool nastavitev_meritev=1;
+
+  static int HITROST_MOTORJA=2000;
+  static int KORAKI_MOTORJA=10000;
 
   //spremenljivki za povprecje:
 
@@ -232,7 +254,7 @@ void ObdelavaPodatkov(bool demo,bool ali_merim, bool odpiranje) {
     nastavitev_meritev=!nastavitev_meritev;
   }
 
-  #if DEBUG
+  #if DEBUG_GENERIC
   Serial.print("Demo argument: ");Serial.println(demo);
   Serial.print("ali_merim argument: ");Serial.println(ali_merim);
   Serial.print("Odpiranje argument: ");Serial.println(odpiranje);
@@ -241,8 +263,15 @@ void ObdelavaPodatkov(bool demo,bool ali_merim, bool odpiranje) {
  // Serial.print("Povprečjeo: ");Serial.println(povprecje);
   
   // demo mode:
-  //if(demo){demo_flag=vrtenje(HITROST_MOTORJA,1,KORAKI-MOTORJA);if(demo_flag)smer!=smer;}  
-  if(demo){/*vrtenje(HITROST_MOTORJA,1,100);*/}
+  if(demo){demo_flag=vrtenje(HITROST_MOTORJA,smer,KORAKI_MOTORJA);
+  if(demo_flag){smer=!smer;
+  #if DEBUG_MOTOR
+  Serial.println("smer swap");
+  #endif
+  demo_flag=0;delay(DEMO_CAKANJE);vrtenje(HITROST_MOTORJA,smer,KORAKI_MOTORJA);}
+  //preventivno se malo zavrti motor v kontra smer, da se ne bi smer takoj zamenjala
+  }  
+  //if(demo){/*vrtenje(HITROST_MOTORJA,1,100);*/}
   else if(ali_merim||odpiranje) //z else if damo prednost demo-tu
   {
   //  Serial.print("Povprečje1: ");Serial.println(povprecje);
@@ -250,13 +279,13 @@ void ObdelavaPodatkov(bool demo,bool ali_merim, bool odpiranje) {
   //za izpis na zaslonu računalnika:
   if(ali_merim){
 
-  #if DEBUG
+  #if DEBUG_MERITVE
   Serial.print("Umirjenost: ");
   Serial.println(umirjenost);
   #endif
 
 
-  #if DEBUG
+  #if DEBUG_MERITVE
   Serial.print("Aktivno povprečje ali povprečje zadnjih 30 meritev? ");
   Serial.println(meritve[ST_POVPRECIJ]!=0); //s tem ve če PC izpise "aktivno povprečje" ali "povprečje zadnjih 30 meritev"
   #endif
@@ -264,12 +293,15 @@ void ObdelavaPodatkov(bool demo,bool ali_merim, bool odpiranje) {
   
   //izracun povprecja:
   
-  if(stevec_meritev==ST_POVPRECIJ){stevec_meritev=0;Serial.println("stevec_meritev reset");}
+  if(stevec_meritev==ST_POVPRECIJ){stevec_meritev=0;
+  #if DEBUG_MERITVE
+  Serial.println("stevec_meritev reset");
+  #endif}
   //mindwave.update(bluetooth,onMindwaveData);
   //umirjenost=mindwave.meditation();
   EEG();
   //umirjenost=meditation;
-  #if DEBUG
+  #if DEBUG_MERITVE
   Serial.print("Umirjenost2: ");
   #endif
   Serial.println(*kazalec);
@@ -277,13 +309,13 @@ void ObdelavaPodatkov(bool demo,bool ali_merim, bool odpiranje) {
   stevec_meritev++;
   for(uint16_t a=0;a<ST_POVPRECIJ;a++)
   {uint16_t povprecje=0;
-    #if DEBUG
+    #if DEBUG_MERITVE
     Serial.print("meritve[a] = ");
     Serial.println(meritve[a]);
     #endif
     if(meritve[a]!=0){povprecje+=meritve[a];}
     else {povprecje/=((a+1)*(a==0)+a*(a>0));
-          #if DEBUG
+          #if DEBUG_MERITVE
           Serial.print("Povprečje: ");
           Serial.println(povprecje);
           #endif
@@ -310,11 +342,26 @@ void regulacija(uint8_t vrednost,bool mode)
 }
 
 //----------------------------------------------------------------------------------------------------------
+void zapiranje_roze()
+{//ta funkcija home-a napravo, torej zapre celo rožo
+  vrtenje(2000,0,1000);
+}
+//----------------------------------------------------------------------------------------------
 //funkcija za vrtenje motorja:
 bool vrtenje(int hitrost, bool smer, int stevilo_korakov)
 {
+  //stevilo_korakov stela rezolucijo? vpliv na čas v for loopu
+static int MAX_POZICIJA_MOTORJA=5000; //vpliva na število obratov v poz smer
 static int stepsPerSecond;
-static int pozicija=0;
+static int pozicija=1;
+static uint32_t micros_prej=0;
+
+#if DEBUG_MOTOR
+Serial.println("vrtim");
+Serial.print("hitrost: ");Serial.print(hitrost);
+Serial.print(" | smer: ");Serial.print(smer);
+Serial.print(" | st_korakov: ");Serial.println(stevilo_korakov);*/
+#endif
 
 // spremenljivka in predznak hitrosti določata če se spremenljivka pozicija povečuje ali zmanjšuje
 if(smer) stepsPerSecond = hitrost;
@@ -322,12 +369,12 @@ else stepsPerSecond = -hitrost;
 
 //for zanka za v specifičnem obsegu:  
 for(int i=stevilo_korakov;i>0;--i)
-{
-   if (((stepsPerSecond > 0) && (pozicija < 9534))||((stepsPerSecond < 0) && (pozicija>0))) //pogoj 9534 je zgornja meja korakov, ki jih naredi motor pri cca 6 obratih
+{//Serial.print("pozicija ");Serial.println(pozicija);
+   if (((stepsPerSecond > 0) && (pozicija < MAX_POZICIJA_MOTORJA))||((stepsPerSecond < 0) && digitalRead(END_SWITCH)/*(pozicija>1)*/)) //pogoj 9534 je zgornja meja korakov, ki jih naredi motor pri cca 6 obratih
    {
     static unsigned long nextChange = 0;
     static uint8_t currentState = LOW;
-
+//Serial.print("hitrost2: ");Serial.println(stepsPerSecond);
     if (stepsPerSecond == 0)
     { //if speed is 0, set the step pin to LOW to keep current position
         currentState = LOW;
@@ -342,8 +389,9 @@ for(int i=stevilo_korakov;i>0;--i)
                 currentState = HIGH;
                 nextChange = micros() + 30;
 
-                if ((stepsPerSecond > 0) && (pozicija < 65535)){pozicija++;}
-                else if ((stepsPerSecond < 0) && (pozicija>0)){pozicija--;}
+                if ((stepsPerSecond > 0) && (pozicija<32348)/*(pozicija < 65535)*/){pozicija++;}
+                else if ((stepsPerSecond < 0) && digitalRead(END_SWITCH)&&(pozicija>-32348)/*(pozicija>1)*/){pozicija--;}
+            //Serial.print("POZICIJA: ");Serial.println(pozicija);
             }
             else{currentState = LOW;nextChange = micros() + (1000 * abs(1000.0f / stepsPerSecond)) - 30;}
 
@@ -355,7 +403,12 @@ for(int i=stevilo_korakov;i>0;--i)
             digitalWrite(PIN_STEP1_STEP, currentState);}
     }}}    
         digitalWrite(PIN_STEP1_STEP, LOW); //dodatna vrstica, preventivno da drži motor pri miru  
-return(pozicija>9534 || digitalRead(END_SWITCH));
+if((!digitalRead(END_SWITCH))&&(stepsPerSecond<0)&&((micros()-micros_prej)>(2000000+DEMO_CAKANJE*1000))){pozicija=0;micros_prej=micros();}
+#if DEBUG_MOTOR
+Serial.print("pogoj1: ");Serial.print((pozicija>MAX_POZICIJA_MOTORJA && smer>0));Serial.print(" | pogoj2: ");Serial.print((pozicija==0 && smer<0));Serial.print(" | pogoj3: ");Serial.println(!digitalRead(END_SWITCH));
+Serial.print("pozicija: ");Serial.print(pozicija);Serial.print(" | hitr: ");Serial.println(stepsPerSecond);
+#endif
+return((pozicija>=MAX_POZICIJA_MOTORJA && stepsPerSecond>0) || (pozicija==0 && stepsPerSecond<0));
         }
 //------------------------------------------------------------------------------------------------
 // funkcija za konfiguracijo glede na nastavitve na UI plošči
@@ -369,7 +422,8 @@ static uint32_t millis_prej=0;
 
 while(!start_gumb)
 {
-if((millis()-millis_prej)>1000)start_gumb=!(digitalRead(START_GUMB));
+//if((millis()-millis_prej)>1000)
+start_gumb=!(digitalRead(START_GUMB));
 roza_stikalo=!digitalRead(ROZA_STIKALO);
 csvlog_stikalo=!digitalRead(CSVLOG_STIKALO);
 demo_stikalo=!digitalRead(DEMO_STIKALO);
@@ -379,7 +433,7 @@ digitalWrite(CSVLOG_LED,csvlog_stikalo);
 digitalWrite(DEMO_LED,demo_stikalo);
 }
 start_gumb=0;
-millis_prej=millis();
+//millis_prej=millis();
 Serial.println((csvlog_stikalo<<1)&(0b010));
 Serial.println((roza_stikalo<<0)&(0b001));
 return (demo_stikalo<<2 | csvlog_stikalo<<1 | roza_stikalo<<0);
